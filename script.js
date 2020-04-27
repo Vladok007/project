3923115876983ff8e760bc3c6b45609c4d5a74f3
VLADOK <95vladok@gmail.com>
2020-04-27 10:31:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+'use scrict';
+let persone = {
+    name: "John",
+    age: 23,
+    loh: true
+};
+
+//console.log (persone['age']);
+//alert("HELLO WORLD!!");
+//let answer = confirm("Are you here?");
+let answer = prompt("Є 18 гівнюк?", "Да");
+
+console.log(answer);
